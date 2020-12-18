c9bf75afb0ad5b914445ab986dd12eb6dd732f63
Sander <sander_m20@hotmail.com>
2020-12-18 21:27:32 +0100

    add devonly

---
+++ index.js
@@ -0,0 +1,82 @@
+const fs = require('fs');
+const Discord = require('discord.js');
+const { prefix, token, devs} = require('./config.json');
+
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    // set a new item in the Collection
+    // with the key as the command name and the value as the exported module
+    client.commands.set(command.name, command);
+}
+
+const cooldowns = new Discord.Collection();
+
+client.once('ready', () => {
+    console.log('Ready!');
+});
+
+client.on('message', message => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const commandName = args.shift().toLowerCase();
+    
+    const command = client.commands.get(commandName) || client.commands.find(cmd => cmd.aliases && cmd.aliases.includes(commandName));
+
+    if (!command) return;
+
+    if (command.devOnly){
+        if (!devs.includes(message.author.id)) {
+            return message.channel.send(`\`${command.name}\` is alleen te gebruiken door de ontwikkelaars van deze bot. ${typeof message.author.id}`)
+        }
+    }
+
+    if (command.args && !args.length) {
+        let reply = `Je hebt geen argumenten opgegeven, ${message.author}!`;
+
+        if (command.usage) {
+            reply += `\nHet juiste gebruik van dit commando is: \`${prefix}${command.name} ${command.usage}\``;
+        }
+
+        return message.channel.send(reply);
+    }
+
+    if (command.guildOnly && message.channel.type === 'dm') {
+        return message.reply(`Ik kan ${command.name} niet uitvoeren in een dm`);
+    }
+
+    if (!cooldowns.has(command.name)) {
+        cooldowns.set(command.name, new Discord.Collection());
+    }
+
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.name);
+    const cooldownAmount = (command.cooldown || 3) * 1000;
+
+    if (timestamps.has(message.author.id)) {
+        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+        if (now < expirationTime) {
+            const timeLeft = (expirationTime - now) / 1000;
+            return message.reply(`wacht nog ${timeLeft.toFixed(1)} seconde(s) voor het opnieuw gebruiken van het \`${command.name}\` command.`);
+        }
+    }
+
+    timestamps.set(message.author.id, now);
+    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
+
+    try {
+        command.execute(message, args);
+    } catch (error) {
+        console.error(error);
+        message.reply('Er is een fout opgetreden bij het uitvoeren van: ' + commandName);
+    }
+    // other commands...
+});
+
+client.login(token);
